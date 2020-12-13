78e3160e0369c8bdc930dfb88582cde28f5cbbf9
Karl Murray <kmurray@insiderealestate.com>
2020-12-12 22:02:21 -0600

    Finished Section 3

--- functions.php
+++ functions.php
@@ -1,11 +1,8 @@
 <?php
-
 /**
  * Return the sum of two numbers
- *
  * @param integer $a The first number
  * @param integer $b The second number
- *
  * @return integer The sum of the two numbers
  */
 function add(int $a, int $b): int
